61c80f89591c32df72b2146ac8a1e99c714fd88d
Harry-Kwesi <harrydegraft@hotmail.com>
2024-01-02 21:30:56 +0530

    added image and background for the home

---
+++ BookAMovie/BookAMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAMovieApp.swift
+//  BookAMovie
+//
+//  Created by Harry Kwesi De Graft on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BookAMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
